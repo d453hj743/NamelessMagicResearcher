a8d3d98bb5d12f4d00715957274c9d923764dda5
容ㄦ <d453hj743@gmail.com>
2026-02-09 16:34:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,17 @@
-const CACHE_NAME = 'nmr-generator-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'tesseract.min.js',
-  'eng.traineddata'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+const CACHE_NAME = 'nmr-v1';
+         const ASSETS = [
+           '/1H-NMR-Gen/',
+           '/1H-NMR-Gen/index.html',
+           '/1H-NMR-Gen/manifest.json',
+           '/1H-NMR-Gen/tesseract.min.js',
+           '/1H-NMR-Gen/icon-192.png',
+           '/1H-NMR-Gen/icon-512.png'
+         ];
+   
+        self.addEventListener('install', (e) => {
+          e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+        });
+   
+        self.addEventListener('fetch', (e) => {
+          e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+        });
